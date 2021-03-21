720ae291368cc44c31a097dca823f5a41ea937b0
NVV <uniondirfolder@gmail.com>
2021-03-21 05:04:30 -0700

    Start

---
+++ wa-asp-mvc-file-operation/Controllers/AuthorizationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wa_asp_mvc_file_operation.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        // GET: Authorization
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ wa-asp-mvc-file-operation/Models/ServerFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wa_asp_mvc_file_operation.Models
+{
+    public class ServerFile
+    {
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+
+        public FileType FileType { get; set; }
+    }
+}
